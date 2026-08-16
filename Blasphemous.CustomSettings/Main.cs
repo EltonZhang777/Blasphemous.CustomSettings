ca0fc86c534a13ecf5e090fae8102eba909a5273
EltonZhang777 <280903158@qq.com>
2026-08-16 22:27:53 +0800

    build: update dependencies

--- Blasphemous.CustomSettings/Main.cs
+++ Blasphemous.CustomSettings/Main.cs
@@ -3,7 +3,7 @@
 namespace Blasphemous.CustomSettings;
 
 [BepInPlugin(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_VERSION)]
-[BepInDependency("Blasphemous.ModdingAPI", "0.1.0")]
+[BepInDependency("Blasphemous.ModdingAPI", "3.0.1")]
 internal class Main : BaseUnityPlugin
 {
     public static CustomSettings CustomSettings { get; private set; }
